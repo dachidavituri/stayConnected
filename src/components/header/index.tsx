f6a823ee9bee761cd679000917a7014085bd5cf0
tinatin92 <tinatygordadze@gmail.com>
2024-11-30 17:33:36 +0400

    header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,61 @@
+
+
+
 import React from "react";
+import Container from "../ui/container";
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import logo from "@/assets/Frame 28.svg";
+import { FiSearch } from "react-icons/fi";
+import { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { Link } from "react-router";
 
 const Header: React.FC = () => {
-  return <div>this is a header</div>;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  return (
+    <header>
+      <Container>
+        <div className="flex items-center py-6 justify-between">
+          <div className="w-20 mr-14">
+            <img src={logo} alt="Logo" />
+          </div>
+          <div
+            className={`burger ${isMenuOpen ? "block" : "hidden"} absolute top-16 right-6 p-6  gap-4 flex flex-col  bg-white shadow-lg 
+          xl:static xl:flex xl:shadow-none xl:flex-row xl:items-center xl:gap-6`}
+          >
+            <div className="flex items-center gap-6">
+              <Input className=" w-48 lg:w-[400px]" />
+              <FiSearch className="w-6" />
+            </div>
+
+            <div className="flex flex-col justify-center items-center lg:flex-row xl:ml-auto gap-6">
+              <Link to="/questions">
+                <Button>Add Question</Button>
+              </Link>
+              <Button variant={"secondary"}>Logout</Button>
+              <Avatar className="rounded-full overflow-hidden">
+                <AvatarImage
+                  className="w-11 h-11 rounded"
+                  src="https://github.com/shadcn.png"
+                />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </div>
+          </div>
+          <div className="xl:hidden ml-auto">
+            <button
+              onClick={() => setIsMenuOpen((prev) => !prev)}
+              className="text-gray-800 focus:outline-none"
+            >
+              <GiHamburgerMenu />
+            </button>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
 };
 
 export default Header;
