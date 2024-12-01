c33cebaa6e92652f2f28c7117f630fcd50924f63
Dachi davituri <davituridachi33@gmail.com>
2024-12-01 20:36:29 +0400

    remove pagination on profile

--- src/pages/profile/views/profileView.tsx
+++ src/pages/profile/views/profileView.tsx
@@ -3,17 +3,6 @@ import Container from "@/components/ui/container";
 import QuestionItem from "@/pages/home/components/questions";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 import { Link } from "react-router";
-import usePagination from "@/hooks/pagination";
-import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from "@/components/ui/pagination";
-import { useState } from "react";
 const QUESTIONITEM_DYMMY_DATA = [
   {
     title: "1 What is Jotaiiiiii?",
@@ -58,33 +47,13 @@ const LIKED_QUESTIONITEM_DYMMY_DATA = [
 ];
 
 const ProfileView: React.FC = () => {
-  const [selectedTab, setSelectedTab] = useState<
-    "askedQuestions" | "likedQuestions"
-  >("askedQuestions");
-  const currentData =
-    selectedTab === "askedQuestions"
-      ? QUESTIONITEM_DYMMY_DATA
-      : LIKED_QUESTIONITEM_DYMMY_DATA;
-  const {
-    currentItems,
-    currentPage,
-    handleNextPage,
-    handlePreviousPage,
-    totalPages,
-  } = usePagination(currentData, 2);
   return (
     <section>
       <Container>
         <Profile />
 
         <div className="">
-          <Tabs
-            defaultValue="askedQuestions"
-            className="w-full"
-            onValueChange={(value) =>
-              setSelectedTab(value as "askedQuestions" | "likedQuestions")
-            }
-          >
+          <Tabs defaultValue="askedQuestions" className="w-full">
             <TabsList className="w-full">
               <TabsTrigger className="w-full" value="askedQuestions">
                 Asked Questions
@@ -94,7 +63,7 @@ const ProfileView: React.FC = () => {
               </TabsTrigger>
             </TabsList>
             <TabsContent value="askedQuestions">
-              {currentItems.map((data, index) => {
+              {QUESTIONITEM_DYMMY_DATA.map((data, index) => {
                 return (
                   <Link to="/" key={index}>
                     <QuestionItem {...data} />
@@ -103,7 +72,7 @@ const ProfileView: React.FC = () => {
               })}
             </TabsContent>
             <TabsContent value="likedQuestions">
-              {currentItems.map((data, index) => {
+              {LIKED_QUESTIONITEM_DYMMY_DATA.map((data, index) => {
                 return (
                   <Link to="/" key={index}>
                     <QuestionItem {...data} />
@@ -114,26 +83,6 @@ const ProfileView: React.FC = () => {
           </Tabs>
         </div>
       </Container>
-      <Pagination className="mt-2">
-        <PaginationContent>
-          <PaginationItem>
-            <PaginationPrevious onClick={handlePreviousPage} />
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#" className="cursor-default">
-              {currentPage}
-            </PaginationLink>
-          </PaginationItem>
-          {currentPage < totalPages && (
-            <PaginationItem>
-              <PaginationEllipsis />
-            </PaginationItem>
-          )}
-          <PaginationItem>
-            <PaginationNext onClick={handleNextPage} />
-          </PaginationItem>
-        </PaginationContent>
-      </Pagination>
     </section>
   );
 };
