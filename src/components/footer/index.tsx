fd9bf72ef55d9bd8321ab3eeaaa412bfd6d69627
tinatin92 <tinatygordadze@gmail.com>
2024-12-01 15:00:48 +0400

    home page

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <div className="flex justify-center items-center bg-slate-300 py-6">
-      this is a footer
+    <div className="flex justify-center items-center bg-slate-950 py-6 text-xl font-semibold text-white">
+      Presented By Team 10
     </div>
   );
 };
