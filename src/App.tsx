c6df403f2e01663caf487b62be227d83fe30d057
Dachi davituri <davituridachi33@gmail.com>
2024-12-03 13:02:14 +0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import DefaultLayout from "@/layouts";
 import SingleQuestionView from "#/singleQuestion/views";
 import ProfileView from "./pages/profile/views/profileView";
 const App: React.FC = () => {
-  // 
+  // hllo
   return (
     <div>
       <Routes>
