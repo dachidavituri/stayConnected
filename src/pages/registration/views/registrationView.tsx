4f524d6e18cb0389dc007e73744311a30aa69b06
tinatin92 <tinatygordadze@gmail.com>
2024-12-05 13:23:59 +0400

    styles

--- src/pages/registration/views/registrationView.tsx
+++ src/pages/registration/views/registrationView.tsx
@@ -1,12 +1,12 @@
 import Card from "@/components/ui/card";
 import Heading from "@/components/ui/heading";
-import logo from "@/assets/Frame 69.svg";
+import logo from "@/assets/Frame 28.svg";
 import Container from "@/components/ui/container";
 import RegisterForm from "#/registration/components";
 const RegistrationView: React.FC = () => {
   return (
     <Container>
-      <section className="register w-full sm:w-[343px] md:w-[400px] flex flex-col gap-11 justify-center items-center m-auto">
+      <section className="register w-full sm:w-[343px] md:w-[400px] flex flex-col gap-11 h-screen justify-center items-center m-auto">
         <div className="">
           <img className="w-28" src={logo} alt="" />
         </div>
